3df9f84e3df05c54311f67c1b90e9d3e7d6d675f
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2026-08-04 09:54:55 +0700

    cmd_role: Change bootcmd to use extlinux_scan

--- common/cmd_role.c
+++ common/cmd_role.c
@@ -47,7 +47,7 @@ int do_role(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
 			debugf("Get chipram env mode %x,go download\n", boot_role);
 			break;
 		case BOOTLOADER_MODE_LOAD:
-			setenv("bootcmd", "cboot;cboot fastboot");
+			setenv("bootcmd", "extlinux_scan");
 			debugf("Get chipram env mode %x,go cboot\n", boot_role);
 			break;
 		default :
