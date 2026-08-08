54b97dcbec1c2eb83d8aa7e80ac2b040d5d46ab8
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2026-08-08 21:16:20 +0700

    loader_nvm: Refactor splash_screen_prepare for logo reading

--- common/loader/loader_nvm.c
+++ common/loader/loader_nvm.c
@@ -467,10 +467,42 @@ void _boot_secure_check(void)
 	return;
 }
 
+/*int splash_screen_prepare(uchar *logo_part_name, u8 *addr)
+{
+	size_t size ;
+	u32 bmp_header_size = 8192;
+
+	size = lcd_get_size(&lcd_line_length);
+	size += bmp_header_size;
+#ifndef CONFIG_ZEBU
+	if (0 != common_raw_read(logo_part_name, (uint64_t)size, (uint64_t)0, addr)) {
+		errorf("failed to read logo partition:%s\n", logo_part_name);
+		return -1;
+	}
+#else
+	memcpy(addr, 0xb0000000, (size_t)size);
+#endif
+
+	return 0;
+}
+*/
+
 int splash_screen_prepare(uchar *logo_part_name, u8 *addr)
 {
 	size_t size ;
 	u32 bmp_header_size = 8192;
+
+	if (logo_part_name && strcmp((char *)logo_part_name, FBOOT_LOGO_PART) == 0) {
+#ifndef CONFIG_ZEBU
+		if (0 != common_raw_read(logo_part_name, (uint64_t)0, (uint64_t)0, addr)) {
+			errorf("failed to read raw bmp partition:%s\n", logo_part_name);
+			return -1;
+		}
+#else
+		memcpy(addr, 0xb0000000, (size_t)size);
+#endif
+		return 0;
+	}
 
 	size = lcd_get_size(&lcd_line_length);
 	size += bmp_header_size;
