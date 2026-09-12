71c406dd2bc4230679a2c47590fd965c78baabe5
Ridhoardhiansyah7 <49192841+Ridhoardhiansyah7@users.noreply.github.com>
2026-09-12 11:47:31 +0700

    lcd: try to fix touchscreen

--- drivers/video/sprd/sprd_panel.c
+++ drivers/video/sprd/sprd_panel.c
@@ -467,9 +467,6 @@ static int of_parse_panel(const void *fdt, int offset, struct panel_info *info)
 	else
 		pr_err("Can't get sprd,lcd-id-tokernel\n");
 
-	str = fdt_getprop(fdt, offset, "sprd,lcd-no-id", &len);
-	if (str)
-		info->lcd_no_id = true;
 
 	/*------Bias Power-------*/
 	ret = fdt_getprop_u32(fdt, offset, "sprd,power-i2c-bus", &val);
@@ -608,7 +605,7 @@ static int of_parse_panel_power(const void *fdt, int offset, struct panel_info *
 		info->power_pin.gpio_tp_reset = val;
 	else
 		pr_err("There is no sprd,tp-reset-gpio", &val);
-	
+
 	return  0;
 }
 
